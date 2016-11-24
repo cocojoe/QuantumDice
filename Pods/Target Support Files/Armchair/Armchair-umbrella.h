125757c1dfad3997c6c749325f8adfbd24ac4bf1
Martin Walsh <martin.walsh@gmail.com>
2016-11-24 23:28:38 +0000

    Testing

--- Pods/Target Support Files/Armchair/Armchair-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "Armchair.h"
-
-FOUNDATION_EXPORT double ArmchairVersionNumber;
-FOUNDATION_EXPORT const unsigned char ArmchairVersionString[];
-
--- Pods/Target Support Files/Pods-QuantumDiceUITests/Pods-QuantumDiceUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QuantumDiceUITests : NSObject
-@end
-@implementation PodsDummy_Pods_QuantumDiceUITests
-@end
